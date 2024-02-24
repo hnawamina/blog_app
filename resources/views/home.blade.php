c4942bf404f1a484847cc83a82b02f42b4286a10
Heshan Nawamina <116290111+heshan1415@users.noreply.github.com>
2024-02-24 14:22:46 +0530

    post creat function created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,16 +17,19 @@
 
                         <div class="">
 
-                            <form>
+                            <form method="POST" action="{{ route('post_store') }}">
+                                @csrf
                                 <div class="form-group ">
                                     <label for="">Post Title</label>
-                                    <input type="text" class="form-control mt-1 bg-white" id=" ">
+                                    <input type="text" name="title" class="form-control mt-1 bg-white" id=" "
+                                        placeholder="Enter Post Title">
                                 </div>
                                 <div class="form-group mt-2 ">
                                     <label for=" ">Post Description</label>
-                                    <textarea type="text" class="form-control mt-1 bg-white" id="" rows="6"></textarea>
+                                    <textarea type="text" name="description" class="form-control mt-1 bg-white" id="" rows="6"
+                                        placeholder="Enter Description"></textarea>
                                 </div>
-                                <button type="submit" class="btn btn-primary mt-2">Submit</button>
+                                <button type="submit" class="btn btn-primary mt-2">Creat Post</button>
                             </form>
 
                         </div>
