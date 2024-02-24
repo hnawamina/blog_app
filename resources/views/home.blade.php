092b5aadada3bf90ba52908ef5945a12ce91d3e1
Heshan Nawamina <116290111+hnawamina@users.noreply.github.com>
2024-02-24 14:40:15 +0530

    blade changes (#2)
    * blade changes
    
    * post creat function created
    
    ---------
    
    Co-authored-by: Heshan Nawamina <116290111+heshan1415@users.noreply.github.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="col-md-6 ">
+                <div class="card">
+                    <div class="card-header">{{ __('Creat Post') }}</div>
+
+                    <div class="card-body bg-white">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="">
+
+                            <form method="POST" action="{{ route('post_store') }}">
+                                @csrf
+                                <div class="form-group ">
+                                    <label for="">Post Title</label>
+                                    <input type="text" name="title" class="form-control mt-1 bg-white" id=" "
+                                        placeholder="Enter Post Title">
+                                </div>
+                                <div class="form-group mt-2 ">
+                                    <label for=" ">Post Description</label>
+                                    <textarea type="text" name="description" class="form-control mt-1 bg-white" id="" rows="6"
+                                        placeholder="Enter Description"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary mt-2">Creat Post</button>
+                            </form>
+
                         </div>
-                    @endif
+                    </div>
 
-                    {{ __('You are logged in!') }}
                 </div>
+
             </div>
+
         </div>
     </div>
-</div>
 @endsection
